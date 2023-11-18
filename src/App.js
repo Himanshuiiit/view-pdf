d14723f9aa1e5e4f2c75fc3314c58b27bc3d93ff
Himanshu Verma <hims6482@gmail.com>
2023-11-18 11:49:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MyApp from "./components/TestComp";
 
 function App() {
+  document.addEventListener("keydown", function (e) {
+    if (
+      (e.ctrlKey || e.metaKey) &&
+      (e.key === "s" || e.key === "S" || e.key === "I")
+    ) {
+      // alert("Saving the PDf is not allowed");
+      e.preventDefault();
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onContextMenu={(e) => e.preventDefault()}>
+      <MyApp />
     </div>
   );
 }
